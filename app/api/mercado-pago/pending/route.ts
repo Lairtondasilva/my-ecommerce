a622fed05b995f12c815bfdcb6ac10baf4feede6
Lairton da Silva <lairtondasilva07@gmail.com>
2025-04-18 21:25:51 -0300

    fix: sucess redirect

--- app/api/mercado-pago/pending/route.ts
+++ app/api/mercado-pago/pending/route.ts
@@ -26,7 +26,7 @@ export async function GET(req: NextRequest) {
   //   aprovado ou diferente de nulo
 
   if (paymentData.status === "approved" || paymentData.date_approved !== null) {
-    return NextResponse.redirect(new URL("sucesss", req.url));
+    return NextResponse.redirect(new URL("/sucesss", req.url));
   }
 
   return NextResponse.redirect(new URL("/", req.url));
